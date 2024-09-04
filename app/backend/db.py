59dff07ded43513349a256d2368972b3f815fb4a
wl9pa <wl9pa-92-92@mail.ru>
2024-09-04 14:37:27 +0300

    Docker Compose - упаковал наше приложение с PostgreSQL и подготовка приложение к деплою, добавив Gunicorn и NGINX

--- app/backend/db.py
+++ app/backend/db.py
@@ -5,7 +5,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 
 # engine = create_engine('sqlite:///ecommerce.db', echo=True)
 # SessionLocal = sessionmaker(bind=engine)
-engine = create_async_engine('postgresql+asyncpg://ecommerce:ecommercep@localhost:5432/ecommerce', echo=True)
+# engine = create_async_engine('postgresql+asyncpg://ecommerce:ecommercep@localhost:5432/ecommerce', echo=True)
+engine = create_async_engine('postgresql+asyncpg://postgres_user:postgres_password@db:5432/postgres_database',
+                             echo=False)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
